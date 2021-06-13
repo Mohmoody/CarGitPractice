44376e1045526a1a7703194fe101c2bb630de506
Mohmoody <syed.mahmoody2014@gmail.com>
2021-06-12 21:55:31 -0400

    the final red audi car

--- src/CarForGit/main.java
+++ src/CarForGit/main.java
@@ -3,6 +3,11 @@ package CarForGit;
 public class main {
     public static void main(String[] args) {
         car audi=new car();
+        audi.door="3";
+        audi.color="Red";
+        audi.model="A8";
+        audi.number="4545asfdg";
+
 
 
 
